60deeefac13757a6db55a77639946ab1fe2d095f
K_Pranav <pranavkarthik1107@gmail.com>
2026-04-03 23:20:55 +0530

    Delete src/src/ALU directory

--- src/src/ALU/subtractor.v
+++
@@ -1,9 +0,0 @@
-// SUbtractor Child-Module
-module fp_subtractor(
-input [31:0] a,
-input [31:0] b,
-output [31:0] result
-);
-wire [31:0] b_neg={~b[31],b[30:0]};
-fp_adder A1(.a(a),.b(b_neg),.result(result));
-endmodule
