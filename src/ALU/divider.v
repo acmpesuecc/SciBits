b495af37487b72b11514cfddd74b14d0b2a6a41c
Keval Pattani <87653668+kevalpattani@users.noreply.github.com>
2026-04-04 12:21:59 +0530

    fix: divide by zero case error
    Test logs:
    
    Time	 OP	 A		 B		 RESULT
    0	 xx	 xxxxxxxx	 xxxxxxxx	 xxxxxxxx
    10000	 00	 3f800000	 40000000	 40400000
    20000	 01	 3f800000	 40000000	 bf800000
    30000	 10	 3f800000	 40000000	 40000000
    40000	 11	 3f800000	 40000000	 3f000000
    50000	 00	 3fc00000	 40200000	 40800000
    60000	 10	 3fc00000	 40200000	 40700000
    70000	 00	 bf800000	 40000000	 3f800000
    80000	 10	 bf800000	 40000000	 c0000000
    90000	 00	 00000000	 40000000	 40000000
    100000	 11	 40000000	 00000000	 7f800000
    110000	 00	 00000001	 00000002	 00000003

--- src/ALU/divider.v
+++ src/ALU/divider.v
@@ -1,21 +1,38 @@
-// divider child module
+`timescale 1ns / 1ps
 module fp_divider(
-input [31:0] a,
-input [31:0] b,
-output [31:0] result
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] result
 );
-wire sign=a[31]^b[31];
-wire [7:0] exp_a=a[30:23];
-wire [7:0] exp_b=b[30:23];
-wire [23:0] mant_a=(exp_a==0)?{1'b0,a[22:0]}:{1'b1,a[22:0]};
-wire [23:0] mant_b=(exp_b==0)?{1'b0,b[22:0]}:{1'b1,b[22:0]};
-wire [47:0] dividend={mant_a,23'b0};
-wire [23:0] mant_div=dividend/mant_b;
+
+wire sign = a[31] ^ b[31];
+wire [7:0] exp_a = a[30:23];
+wire [7:0] exp_b = b[30:23];
+wire [23:0] mant_a = (exp_a == 0) ? {1'b0, a[22:0]} : {1'b1, a[22:0]};
+wire [23:0] mant_b = (exp_b == 0) ? {1'b0, b[22:0]} : {1'b1, b[22:0]};
+wire [47:0] dividend = {mant_a, 23'b0};
+wire [23:0] mant_div = dividend / mant_b;
+
 reg [7:0] exp_r;
 reg [22:0] mant_r;
+reg [23:0] mant_norm;
+
 always @(*) begin
-mant_r=mant_div[22:0];
-exp_r=exp_a-exp_b+8'd127;
+    if (b[30:0] == 0) begin
+        exp_r  = 8'hFF;
+        mant_r = 23'b0; // INF
+    end
+    else begin
+        exp_r = exp_a - exp_b + 8'd127;
+        mant_norm = mant_div;
+        if (mant_norm[23] == 0) begin
+            mant_norm = mant_norm << 1;
+            exp_r = exp_r - 1;
+        end
+        mant_r = mant_norm[22:0];
+    end
 end
-assign result={sign,exp_r,mant_r};
+
+assign result = {sign, exp_r, mant_r};
+
 endmodule
